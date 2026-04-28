eada01b513fe8dbd3de625d62ebc6e451287947b
Kim Robbens <kim.robbens@gmail.com>
2026-04-28 20:44:06 +0200

    Update PowerShell script path in install.bat

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,5 +2,5 @@ rem force a batch file to operate within its own directory rather than C:\Window
 @echo off
 cls
 cd /d "%~dp0"
-powershell -ExecutionPolicy Bypass scripts\install.ps1
+powershell -ExecutionPolicy Bypass install\install.ps1
 pause
